f864477b2a11a4874f2093cc683c95319616cdf4
ayushrawat98 <aayushrawat0@gmail.com>
2020-06-16 11:41:39 +0530

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,4 @@
 var CACHE_NAME = 'static-cache';
-var urlsToCache = [
-  '/',
-  '/index.html',
-  '/creatine.html',
-  '/dietpage.html',
-  '/gainer.html',
-  '/glutamine.html',
-  '/Nutrition_needed.html',
-  '/protein.html',
-  '/secpage.html',
-  '/thirdpage.html',
-];
-
-
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
