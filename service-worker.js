47a687f27cb422f408f0bd9e47cbae5650a988cb
ayushrawat98 <aayushrawat0@gmail.com>
2020-06-16 11:34:10 +0530

    fix

--- service-worker.js
+++ service-worker.js
@@ -13,40 +13,40 @@ var urlsToCache = [
 ];
 
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-      .then(function(response) {
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
         return response || fetchAndCache(event.request);
       })
-    );
-  });
-  
-  function fetchAndCache(url) {
-    return fetch(url)
-    .then(function(response) {
+  );
+});
+
+function fetchAndCache(url) {
+  return fetch(url)
+    .then(function (response) {
       // Check if we received a valid response
       if (!response.ok) {
         throw Error(response.statusText);
       }
       return caches.open(CACHE_NAME)
-      .then(function(cache) {
-        cache.put(url, response.clone());
-        return response;
-      });
+        .then(function (cache) {
+          cache.put(url, response.clone());
+          return response;
+        });
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.log('Request failed:', error);
       // You could return a custom offline 404 page here
     });
-  }
+}
